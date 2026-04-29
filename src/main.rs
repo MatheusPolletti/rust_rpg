dc8553f179e89550f38af5e694bc4c10ad709998
Matheus Polletti <matheus.c.polletti@gmail.com>
2026-04-29 11:52:55 -0300

    implement user greeting and name input validation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let name_user: String = greetings();
+}
+
+fn greetings() -> String {
+    println!("Bem-vindo ao RPG SB Tecnologia.");
+
+    let mut name = String::new();
+    let stdin = io::stdin();
+
+    loop {
+        println!("Adicione o seu nome:");
+
+        name.clear();
+
+        stdin.read_line(&mut name).expect("Erro ao ler a linha");
+
+        let name = name.trim();
+
+        let total_letters_name = name.chars().count();
+
+        match total_letters_name {
+            n if n < 2 => {
+                println!("Nome muito curto, precisa de pelo menos 2 dígitos");
+            }
+            2..=100 => {
+                return name.to_string();
+            }
+            _ => {
+                println!("Nome muito longo, no máximo 100 dígitos",);
+            }
+        }
+    }
 }
